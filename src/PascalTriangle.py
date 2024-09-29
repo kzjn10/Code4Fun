a61593b73446480a5aa0d1e9aa7459b128da4941
AnhNDT <anhnguyen.lk@gmail.com>
2024-09-29 09:09:29 -0500

    Create PascalTriangle.py

---
+++ src/PascalTriangle.py
@@ -0,0 +1,19 @@
+# 118. Pascal's triangle
+def pascal_triangle(num_rows: int):
+    result = []
+    # Start at 1 to num rows + 1 to avoid first list empty
+    for i in range(1, num_rows + 1):
+        # Create new array with default value 1
+        row = [1] * i
+        # Loop j in range i
+        for j in range(i):
+            # Calculator item difference first and last
+            if 0 < j < (i - 1):
+                # Get position of last row and top left + top right
+                row[j]= result[i - 2][j - 1] + result[i - 2][j]
+        # Add row
+        result.append(row)
+    return result
+
+
+print(pascal_triangle(5))
